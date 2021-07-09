415d6c87a2279c708e49e78fdfbf9559e60ab2a8
daria-bondarenko <dbondarenko.exceedteam@gmail.com>
2021-07-09 09:18:48 +0300

    create regi8stration page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React from "react";
+import {
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom';
+import Header from "./components/Header/Header";
+import Registration from "./components/Registration/Registration";
 import './App.css';
 
-function App() {
+const App = () => {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route path='/logIn'>
+          <Header title={'Войти в систему'} button={false}/>
+
+        </Route>
+        <Route path='/signUp'>
+          <Header title={'Зарегистрироваться в системе'} button={false}/>
+          <Registration/>
+        </Route>
+        <Route path='/tricks'
+               render={() => {
+                 return (localStorage.getItem('token')) ? (
+                   <div>
+                   </div>
+                 ) : (
+                   <Redirect from="/tricks" to="/logIn"/>
+                 );
+               }}
+        />
+        <Redirect to='/login' from='/'/>
+      </Switch>
     </div>
   );
 }
 
 export default App;
+
